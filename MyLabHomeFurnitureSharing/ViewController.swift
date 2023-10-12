af9605a505dfa3fdc3fb35e999fe95acde1ff044
wayne tsao <a578ff6@gmail.com>
2023-10-12 14:49:01 +0800

    Initial Commit

---
+++ MyLabHomeFurnitureSharing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLabHomeFurnitureSharing
+//
+//  Created by 曹家瑋 on 2023/10/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
